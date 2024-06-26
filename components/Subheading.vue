87f1604dad6df513fb3b3a657c7e99c313cad796
Piotr Drechsler <pdrech@gmail.com>
2024-06-26 12:58:28 +0200

    add basic strusture and prop of component

---
+++ components/Subheading.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="subheading__wrapper">
+    <h2 class="subheading__header">{{ props.name }}</h2>
+  </div>
+</template>
+
+<style>
+.subheading__wrapper {
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 1rem;
+  text-align: center;
+}
+
+.subheading__header {
+  font-size: 2rem;
+  font-weight: bold;
+  color: var(--primary-color);
+  margin-bottom: 1rem;
+  transition: 0.2s;
+}
+
+</style>
