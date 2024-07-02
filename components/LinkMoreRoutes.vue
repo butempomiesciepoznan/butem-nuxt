b07c24b71ecb11dda00f91bdde5ba8133b21b75a
Piotr Drechsler <pdrech@gmail.com>
2024-07-02 18:51:15 +0200

    modified:   assets/css/_colors.css added:   components/LinkMoreRoutes.vue

--- components/LinkMoreRoutes.vue
+++ components/LinkMoreRoutes.vue
@@ -1,9 +1,31 @@
 <script setup>
-
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  href: {
+    type: String,
+    required: true
+  },
+  rel: {
+    type: String,
+    default: 'noopener noreferrer'
+  }
+});
 </script>
 
 <template>
-   <p>TEST</p>
-</template>
+   <v-btn :href="href" :rel="rel" block variant="flat" rounded="lg" density="default" class="text-none mt-6"  >
+      {{ name }}
+   </v-btn>
+ </template>
 
-
+<style scoped>
+.v-btn {
+  background: var(--grey-backgroud);
+  letter-spacing: 0.1px;
+  line-height: 20px;
+  font-weight: 600;
+}
+</style>
