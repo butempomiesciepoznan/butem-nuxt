6a71a40c37b42ce411619446d6420d5957b85115
Piotr Drechsler <105511338+PiotrDrechsler@users.noreply.github.com>
2024-07-04 20:33:04 +0200

    KMP-2 Add "LinkMoreRoutes" button component (#11)
    * Create basic component
    
    * modified:   assets/css/_colors.css
    added:   components/LinkMoreRoutes.vue
    
    * Remove noreferrer for better page  optimalization
    
    * Create global css vars for letter spacing and line height for all buttons

---
+++ components/LinkMoreRoutes.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  href: {
+    type: String,
+    required: true
+  },
+  rel: {
+    type: String,
+    default: 'noopener'
+  }
+});
+</script>
+
+<template>
+   <v-btn :href="href" :rel="rel" block variant="flat" rounded="lg" density="default" class="text-none mt-6"  >
+      {{ name }}
+   </v-btn>
+ </template>
+
+<style scoped>
+.v-btn {
+  background: var(--grey-backgroud);
+  letter-spacing: var(--button-letter-spacing);
+  line-height: var(--button-line-height);
+  font-weight: 600;
+}
+</style>
