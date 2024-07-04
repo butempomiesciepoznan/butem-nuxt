7742340136e14291a862f5bccb38c27a895b2943
Piotr Drechsler <pdrech@gmail.com>
2024-07-04 21:00:06 +0200

    add basic structure of component

---
+++ components/TagsMain.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+  tags: {
+    type: Array,
+    required: true,
+    validator: (value) => value.every(tag => 'name' in tag && 'icon' in tag)
+  },
+});
+</script>
+
+<template>
+   <p>TEST</p>
+ </template>
+
+<style scoped>
+
+
+</style>
