b691420e711136adea17df73cc6b6f45ecfe182b
Piotr Drechsler <105511338+PiotrDrechsler@users.noreply.github.com>
2024-07-09 14:02:47 +0200

    KMP-6 Add component to generate tags on main page (#12)
    * add basic structure of component
    
    * rename the component to something more understandable
    
    * rename the component to something more understandable
    
    * Basic styling for a single component such as a single tag on a figma
    
    * Add local changes
    
    * create logic to map array with tags names and icons and render them
    
    * add variables in colors for tag component

---
+++ components/TagMainPage.vue
@@ -0,0 +1,39 @@
+<script setup>
+const props = defineProps({
+  tags: {
+    type: Array,
+    required: true,
+    validator: (value) => value.every((tag) => "name" in tag && "icon" in tag),
+  },
+});
+</script>
+
+<template>
+  <div class="tag-container">
+    <v-chip
+      v-for="(tag, index) in props.tags"
+      :key="index"
+      :append-icon="tag.icon"
+      variant="flat"
+      size="large"
+    >
+      {{ tag.name }}
+    </v-chip>
+  </div>
+</template>
+
+<style scoped>
+.tag-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  margin: 25px 0;
+}
+.v-chip {
+  background-color: var(--light-grey-background);
+  color: var(--grey-tag);
+  max-height: 24px;
+}
+</style>
