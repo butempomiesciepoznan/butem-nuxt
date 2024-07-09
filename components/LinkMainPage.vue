e317edd398efb53d965e9b3240d9f3604d0fa844
Piotr Drechsler <pdrech@gmail.com>
2024-07-09 19:15:41 +0200

    add styling for component, like on Figma template

--- components/LinkMainPage.vue
+++ components/LinkMainPage.vue
@@ -7,12 +7,18 @@ const props = defineProps({
 <template>
   <div class="container">
     <v-icon icon="mdi-arrow-left" />
-    <h2>{{ cityName }}</h2>
+    <h2 v-cloak class="heading">{{ cityName }}</h2>
   </div>
 </template>
 
 <style scoped>
 .container {
   display: flex;
+  align-items: center;
+  padding: 12px 12px 16px 4px;
+}
+.heading {
+  font-size: 22px;
+  margin-left: 15px;
 }
 </style>
