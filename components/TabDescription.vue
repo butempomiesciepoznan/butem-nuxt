3248d28662ab10eb4f2b4b85e74e49696fe5b504
Piotr Drechsler <pdrech@gmail.com>
2024-07-12 19:32:56 +0200

    create two components: Tabs to render Tabs, and TabDescription to generete single Tab with description prop

---
+++ components/TabDescription.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const { description } = props;
+</script>
+
+<template>
+  <div>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<style scoped></style>
--- components/Tabs.vue
+++ components/Tabs.vue
@@ -1,27 +1,43 @@
+<script setup>
+const tab = ref("one");
+</script>
+
 <template>
   <v-card>
-    <v-tabs v-model="tab" bg-color="primary">
-      <v-tab value="one">Item One</v-tab>
-      <v-tab value="two">Item Two</v-tab>
-      <v-tab value="three">Item Three</v-tab>
+    <v-tabs v-model="tab">
+      <v-tab value="description">Opis</v-tab>
+      <v-tab value="map">Mapa</v-tab>
+      <v-tab value="routes">Trasy</v-tab>
     </v-tabs>
 
     <v-card-text>
       <v-tabs-window v-model="tab">
-        <v-tabs-window-item value="one"> One </v-tabs-window-item>
+        <v-tabs-window-item value="description">
+          <TabDescription description="opis testowy" />
+        </v-tabs-window-item>
 
-        <v-tabs-window-item value="two"> Two </v-tabs-window-item>
+        <v-tabs-window-item value="map"> Mapa </v-tabs-window-item>
 
-        <v-tabs-window-item value="three"> Three </v-tabs-window-item>
+        <v-tabs-window-item value="routes"> Trasy </v-tabs-window-item>
       </v-tabs-window>
     </v-card-text>
   </v-card>
 </template>
 
-<script>
-export default {
-  data: () => ({
-    tab: null,
-  }),
-};
-</script>
+<style scoped>
+/* Add any custom styles here */
+.v-card {
+  margin: 20px auto;
+}
+
+.v-tabs {
+  background-color: var(--light-grey-background);
+}
+.v-tab {
+  font-size: 1rem;
+  text-transform: capitalize;
+  color: var(--darken-grey);
+  letter-spacing: var(--button-letter-spacing);
+  line-height: var(--button-line-height);
+}
+</style>
