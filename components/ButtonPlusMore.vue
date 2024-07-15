af4b938f9717eca52b31b2a74860428addc63681
Sabi_K <sabiku85@gmail.com>
2024-07-15 22:23:50 +0200

    defineEmits

--- components/ButtonPlusMore.vue
+++ components/ButtonPlusMore.vue
@@ -1,15 +1,17 @@
 <script setup>
 let isShorter = ref(true)
 
+const emit = defineEmits(['increase-text', 'decrease-text'])
 const toggleText = () => isShorter.value = !isShorter.value
+const emitEvents = () => isShorter.value ? emit('decrease-text') : emit('increase-text')
 </script>
 
 <template>
   <v-btn-toggle class="flex items-center justify-center">
-    <v-btn v-show="isShorter" class="text-lowercase" variant="text" prepend-icon="mdi-plus" @click="toggleText">
+    <v-btn v-show="isShorter" class="text-lowercase" variant="text" prepend-icon="mdi-plus" @click="[toggleText($event),emitEvents($event)]">
       więcej
     </v-btn>
-    <v-btn v-show="!isShorter" class="text-lowercase" variant="text" prepend-icon="mdi-minus" @click="toggleText">
+    <v-btn v-show="!isShorter" class="text-lowercase" variant="text" prepend-icon="mdi-minus" @click="[toggleText($event), emitEvents($event)]">
       mniej
     </v-btn>
   </v-btn-toggle>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -19,11 +19,19 @@ const { data: pages, pending, error, refresh } = await useFetch(`${config.public
   }
 )
 
+const toggleTextLength = (height) => {
+  return document.querySelector('[data-text-length]').style.height = height + 'px'
+}
+
 </script>
 <template>
   <UContainer>
     <div v-for="page in pages" :key="page.slug">
       <Header :title="page.title.rendered" :content="page.content.rendered" />
+
+      <div style="width:300px; height:300px;background-color: aqua; transition: 0.4s height" data-text-length>tekst</div>
+      <ButtonPlusMore @increase-text="toggleTextLength(400)" @decrease-text="toggleTextLength(300)" />
+      
     </div>
   </UContainer>
 </template>
