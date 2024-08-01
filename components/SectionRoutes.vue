f6a720fd119d7cac585beaa63122fa6bfba7dde3
Sabi_K <sabiku85@gmail.com>
2024-08-02 00:30:57 +0200

    SG-2 Add routes section with component for slider with description

---
+++ components/SectionRoutes.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  images: {
+    type: Array,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+  }
+});
+
+const { name, images } = props;
+</script>
+
+<template>
+  <Subheading :name="name" />
+  <CarouselWithDescription :images="images" />
+</template>
