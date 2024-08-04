cf7b702451fbeaee179e9dbc24c6b232b5a843e7
Sabi_K <sabiku85@gmail.com>
2024-08-04 18:03:51 +0200

    Adjustments to tailwindcss classes for mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@ let { isMobile } = useDeviceType()
 </script>
 
 <template>
-  <div v-cloak class="tw-mt-0 sm:tw-mt-16">
+  <div v-cloak class="md:tw-mt-16 tw-mt-0 tw-mx-4 md:tw-mx-0">
     <TheMainNav v-if="!isMobile"></TheMainNav>
     <slot />
     <TheFooterNav v-if="!isMobile"></TheFooterNav>
